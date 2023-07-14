da71cb8cdff808d006a9d3957cfd510fa77868cd
jakub <jakubrogulski01@gmail.com>
2023-07-14 14:25:37 +0200

    add tests for metrics

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -4,13 +4,37 @@ from utils.metrics import *
 class TestMetrics(unittest.TestCase):
     
     def setUp(self):
-        self.y_true = np.array([0,0,0,0,1,0,0,1,1,1])
-        self.y_pred= np.array([0,0,0,0,0,1,1,1,1,1])
+        self.cat_y_true = np.array([0,0,0,0,1,0,0,1,1,1])
+        self.cat_y_pred= np.array([0,0,0,0,0,1,1,1,1,1])
+        self.lin_y_true = np.array([100, -100, 0, 200])
+        self.lin_y_pred = np.array([-100, -100, -100, -100])
 
     def test_accuracy(self):
-        results = accuracy(self.y_true, self.y_pred)
-        self.assertEqual(results, 0.7)
+        results = accuracy(self.cat_y_true, self.cat_y_pred)
 
+        self.assertAlmostEqual(results, 0.7, places=7)
+
+    def test_mean_square_error(self):
+        mse = mean_square_error(self.lin_y_true, self.lin_y_pred)
+        rmse = mean_square_error(self.lin_y_true, self.lin_y_pred, root=True)
+        self.assertAlmostEqual(mse, 35000.0, places=7)
+        self.assertAlmostEqual(rmse, 187.08286933869707, places=7)
+        
+    def test_mean_absolute_error(self):
+        result = mean_absolute_error(self.lin_y_true, self.lin_y_pred)
+        self.assertAlmostEqual(result, 150.0, places=7)
+
+    def test_recall(self):
+        result = recall(self.cat_y_true, self.cat_y_pred)
+        np.testing.assert_allclose(result, np.array([0.66666667, 0.75]), rtol=1e-03)
+
+    def test_precision(self):
+        result = precision(self.cat_y_true, self.cat_y_pred)
+        np.testing.assert_allclose(result, np.array([0.8, 0.6]), rtol=1e-03)
+
+    def test_f1_score(self):
+        result = f1_score(self.cat_y_true, self.cat_y_pred)
+        np.testing.assert_allclose(result, np.array([0.72727272, 0.66666666]), rtol=1e-03)
 
 if __name__ == "__main__":
     unittest.main()
