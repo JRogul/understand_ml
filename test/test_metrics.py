152734fc54265c990c03d35f6d2a40d4d69df74d
jakub <jakubrogulski01@gmail.com>
2023-07-15 11:13:27 +0200

    add more robust tests with sklearn

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from utils.metrics import *
+from sklearn import metrics
 class TestMetrics(unittest.TestCase):
     
     def setUp(self):
@@ -17,24 +18,25 @@ class TestMetrics(unittest.TestCase):
     def test_mean_square_error(self):
         mse = mean_square_error(self.lin_y_true, self.lin_y_pred)
         rmse = mean_square_error(self.lin_y_true, self.lin_y_pred, root=True)
-        self.assertAlmostEqual(mse, 35000.0, places=7)
-        self.assertAlmostEqual(rmse, 187.08286933869707, places=7)
+        self.assertAlmostEqual(mse, metrics.mean_squared_error(self.lin_y_true, self.lin_y_pred), places=7)
+        self.assertAlmostEqual(rmse, metrics.mean_squared_error(self.lin_y_true, self.lin_y_pred, squared=False), places=7)
         
     def test_mean_absolute_error(self):
         result = mean_absolute_error(self.lin_y_true, self.lin_y_pred)
-        self.assertAlmostEqual(result, 150.0, places=7)
+        result = mean_absolute_error(self.lin_y_true, self.lin_y_pred)
+        self.assertAlmostEqual(result, metrics.mean_absolute_error(self.lin_y_true, self.lin_y_pred), places=7)
 
     def test_recall(self):
         result = recall(self.cat_y_true, self.cat_y_pred)
-        np.testing.assert_allclose(result, np.array([0.66666667, 0.75]), rtol=1e-03)
+        np.testing.assert_allclose(result, metrics.recall_score(self.cat_y_true, self.cat_y_pred, average=None), rtol=1e-03)
 
     def test_precision(self):
         result = precision(self.cat_y_true, self.cat_y_pred)
-        np.testing.assert_allclose(result, np.array([0.8, 0.6]), rtol=1e-03)
+        np.testing.assert_allclose(result, metrics.precision_score(self.cat_y_true, self.cat_y_pred, average=None), rtol=1e-03)
 
     def test_f1_score(self):
         result = f1_score(self.cat_y_true, self.cat_y_pred)
-        np.testing.assert_allclose(result, np.array([0.72727272, 0.66666666]), rtol=1e-03)
+        np.testing.assert_allclose(result, metrics.f1_score(self.cat_y_true, self.cat_y_pred, average=None), rtol=1e-03)
 
 if __name__ == "__main__":
     unittest.main()
